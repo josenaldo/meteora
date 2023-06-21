7623ae5a2fa761b9e7704f8ec37a4011d427ba29
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-06-21 18:25:48 -0300

    :recycle: refactor: Refactor of logo

---
+++ src/features/hooks/use-device.js
@@ -0,0 +1,33 @@
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+const devices = {
+  xs: 'mobile',
+  sm: 'tablet',
+  md: 'desktop',
+  lg: 'desktop',
+  xl: 'desktop',
+}
+
+export default function useDevice() {
+  const theme = useTheme()
+
+  const matches = {
+    xs: useMediaQuery(theme.breakpoints.up('xs')),
+    sm: useMediaQuery(theme.breakpoints.up('sm')),
+    md: useMediaQuery(theme.breakpoints.up('md')),
+    lg: useMediaQuery(theme.breakpoints.up('lg')),
+    xl: useMediaQuery(theme.breakpoints.up('xl')),
+  }
+
+  const keys = Object.keys(matches).reverse()
+
+  const width = keys.reduce((output, key) => {
+    if (matches[key] && !output) {
+      output = devices[key]
+    }
+    return output
+  }, null)
+
+  return width
+}
--- src/features/layout/Header/Logo.jsx
+++ src/features/layout/Header/Logo.jsx
@@ -2,36 +2,39 @@
 import { Box } from '@mui/material'
 import Image from 'next/image'
 
-import logoDesktop from '@/assets/images/logo-desktop.png'
-import logoMobile from '@/assets/images/logo-mobile.png'
-import logoTablet from '@/assets/images/logo-tablet.png'
+import { useDevice } from '@/features/hooks'
+
+const logoSizes = {
+  desktop: {
+    width: 132,
+    height: 32,
+  },
+  tablet: {
+    width: 100,
+    height: 24,
+  },
+  mobile: {
+    width: 126,
+    height: 18,
+  },
+}
 
 export default function Logo() {
+  const device = useDevice()
+
+  const { width, height } = logoSizes[device]
+
   return (
-    <Box>
-      <Box
-        component={Image}
-        src={logoDesktop}
+    <Box
+      sx={{
+        position: 'relative',
+      }}
+    >
+      <Image
+        src={`/assets/images/${device}/logo.png`}
         alt="logo"
-        sx={{
-          display: { xs: 'none', sm: 'none', md: 'block' },
-        }}
-      />
-      <Box
-        component={Image}
-        src={logoTablet}
-        alt="logo"
-        sx={{
-          display: { xs: 'none', sm: 'block', md: 'none' },
-        }}
-      />
-      <Box
-        component={Image}
-        src={logoMobile}
-        alt="logo"
-        sx={{
-          display: { xs: 'block', sm: 'none', md: 'none' },
-        }}
+        width={width}
+        height={height}
       />
     </Box>
   )
