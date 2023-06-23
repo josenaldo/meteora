71ddfa34267ae9b5ec78587c24948f3363519b9e
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-06-23 19:36:33 -0300

    :construction: fix: Collecting data about deploy

--- src/app/@categories/page.js
+++ src/app/@categories/page.js
@@ -1,36 +1,9 @@
-import { CategoryList } from '@/features/category'
+'use client'
 
-async function getCategories() {
-  const url = `${process.env.NEXT_PUBLIC_API_URL}/categories`
-  console.log('🟢 URL', url)
-
-  const response = await fetch(url)
-  console.log('🟢 Response', response)
-
-  if (!response.ok) {
-    console.log('🔴 Response not ok', response)
-    throw new Error(response.statusText)
-  }
-
-  const data = await response.json()
-  console.log('🟢 Data', data)
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(data)
-    }, 2000)
-  })
-}
+import { CategoryList, getCategories } from '@/features/category'
 
 export default async function CategoryBlock() {
   const categories = await getCategories()
 
-  // return <CategoryList categories={categories} />
-  return (
-    <ul>
-      {categories.map((cat) => (
-        <li key={cat.name}>{cat.name}</li>
-      ))}
-    </ul>
-  )
+  return <CategoryList categories={categories} />
 }
